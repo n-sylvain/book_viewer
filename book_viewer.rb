eb9fb418f5c1ea7a80f2fc1c661fdbe8f488f3f8
Sylvain Ni <sylvainni@msn.com>
2024-04-25 11:33:58 +0200

    Code challenge dynamic file list

--- book_viewer.rb
+++ book_viewer.rb
@@ -5,9 +5,16 @@ require "sinatra/reloader"
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
   @contents = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp1.txt")
 
   erb :home
+end
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @contents = File.readlines("data/toc.txt")
+  @chapter = File.read("data/chp1.txt")
+
+  erb :chapter
 end
 
 # get "/" do
