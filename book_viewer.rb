6dcd7c800dd53d678738387f744fbf9f799ee66d
Sylvain Ni <sylvainni@msn.com>
2024-04-25 11:20:25 +0200

    Adding Table of contents

--- book_viewer.rb
+++ book_viewer.rb
@@ -3,6 +3,10 @@ require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
+  @title = "The Adventures of Sherlock Holmes"
+  @contents = File.readlines("data/toc.txt")
+  @chapter = File.read("data/chp1.txt")
+
   erb :home
 end
 
