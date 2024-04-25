c4e1571ca35503e94b0c573a8e25bc9d56ca5c13
Sylvain Ni <sylvainni@msn.com>
2024-04-25 11:08:29 +0200

    Adding erb

--- book_viewer.rb
+++ book_viewer.rb
@@ -1,9 +1,14 @@
+require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-  File.read "public/template.html"
+  erb :home
 end
+
+# get "/" do
+#   File.read "public/template.html"
+# end
 
 # get "/" do
 #   "Hello World!"
